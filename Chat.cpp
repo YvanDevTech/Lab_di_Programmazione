5c6004142d80cde965ab272aee1fa6b8e38852a1
YvanDevTech <yvantakoukam@gmail.com>
2024-01-05 14:45:37 +0100

    implementazione di readMessage; cancellazione di getMessage()

--- Chat.cpp
+++ Chat.cpp
@@ -5,12 +5,12 @@
 #include "Chat.h"
 
 void Chat::addMessage(const Message &msg) {
-    if (msg.getAddressee() != user1 && msg.getAddressee() != user2)
+    if (msg.getAddressee() != person1 && msg.getAddressee() != person2)
         throw std::invalid_argument("Messaggio non corrispondente a nessun user.");
     messages.emplace_back(msg.getSender(), msg.getAddressee(), msg.getText());
 }
 
-std::_List_iterator<Message> Chat::firstUnreadMessage() {
+std::_List_iterator<Message> Chat::firstUnreadMessage() const {
     auto result = messages.end();
     for (auto it = messages.begin(); it != messages.end(); it++) {
         if (!it->isRead()) {
@@ -21,19 +21,19 @@ std::_List_iterator<Message> Chat::firstUnreadMessage() {
     return result;
 }
 
-void Chat::open() {
-    std::cout << "Hai aperto una messaggeria tra " << user1 << " e " << user2 << std::endl;
+/*void Chat::open() {
+    std::cout << "Hai aperto una messaggeria tra " << person1 << " e " << person2 << std::endl;
     for (auto it = firstUnreadMessage(); it != messages.end(); it++) {
         it->setRead(true);
     }
-}
+}*/
 
 void Chat::deleteAll() {
     messages.clear();
 }
 
 void Chat::deleteMsg(int msgPos) {
-    if (msgPos > messages.size() || msgPos < 0)
+    /*if (msgPos > messages.size() || msgPos < 0)
         throw std::out_of_range("Il messaggio selezionato non esiste.");
 
     auto it = messages.begin();
@@ -42,24 +42,40 @@ void Chat::deleteMsg(int msgPos) {
         it++;
         i++;
     }
-    messages.erase(it);
+    messages.erase(it);*/
+    if (msgPos >= messages.size() || msgPos < 0)
+        throw std::out_of_range("Il messaggio selezionato non esiste.");
+
+    messages.erase(messages.begin() + msgPos);
 }
 
-const std::string &Chat::getUser1() const {
-    return user1;
+const std::string &Chat::getUser1()   const {
+    return person1;
 }
 
 
 const std::string &Chat::getUser2() const {
-    return user2;
+    return person2;
 }
 
-const std::list<Message> *Chat::getMessages() const {
-    return &messages;
+void Chat::readMessage(int msgPos) const {
+    if (msgPos >= messages.size() || msgPos < 0)
+        throw std::out_of_range("Il messaggio selezionato non esiste.");
+
+    auto it = messages.begin();
+    std::advance(it, msgPos);
+
+    std::cout << "Mittente: " << it->getSender() << std::endl;
+    std::cout << "Testo: " << it->getText() << std::endl;
+    std::cout << "Letto: " << (it->isRead() ? "Sì" : "No") << std::endl;
 }
+
+/*const std::list<Message> *Chat::getMessages() const {
+    return &messages;
+}*/
 
 bool Chat::operator==(const Chat &rhs) const {
-    return (rhs.user1 == user1 && rhs.user2 == user2);
+    return (rhs.person1 == person1 && rhs.person2 == person2);
 }
 
 bool Chat::operator!=(const Chat &rhs) const {
