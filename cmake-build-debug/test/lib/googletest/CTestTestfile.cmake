b16cbe15b544f6cef636ec320ff7fd18a5b78fe9
YvanDevTech <yvantakoukam@gmail.com>
2024-01-05 01:48:01 +0100

    Cancellare cmake-build-debug dal versionamento

--- cmake-build-debug/test/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/39351/CLionProjects/Project-Chat/test/lib/googletest
-# Build directory: C:/Users/39351/CLionProjects/Project-Chat/cmake-build-debug/test/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/test/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/39351/CLionProjects/Project-Chat/test/lib/googletest/googletest
-# Build directory: C:/Users/39351/CLionProjects/Project-Chat/cmake-build-debug/test/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
