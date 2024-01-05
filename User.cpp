6b5b9419aa2030ebf11b0813d16c0579d4067280
YvanDevTech <yvantakoukam@gmail.com>
2024-01-05 13:16:33 +0100

    modifica di startnewChat e removeChat, rendere un boolean nel caso in cui si inizia una nuova massaggeria e nel caso in cui si rimuove una chat

--- User.cpp
+++ User.cpp
@@ -29,14 +29,16 @@ bool User::isRegisterEmpty() const {
 
 void User::startNewChat(User &otherUser) {
     Chat newChat(nick, otherUser.nick);
-    auto &chatList = chatRegister.getChats();
-    auto &othersChatList = otherUser.chatRegister.getChats();
+    auto &chatList = chatRegister.chats;
+    auto &othersChatList = otherUser.chatRegister.chats;
+
     if (chatList.empty() || chatList.find(otherUser.nick) == chatList.end()) {
         auto chatShared = chatRegister.addChat(otherUser.nick, newChat);
         othersChatList.emplace(nick, chatShared);
-
-    } else
-        throw std::invalid_argument("La messaggeria con questo user esiste gia.");
+        return true;  // La messaggeria è stata avviata con successo
+    } else {
+        return false;  // La messaggeria con questo utente esiste già
+    }
 }
 
 void User::addChat(const User &otherUser, const Chat &newChat) {
@@ -45,13 +47,20 @@ void User::addChat(const User &otherUser, const Chat &newChat) {
 
 void User::removeChat(const std::string &username) {
 
-    chatRegister.removeChat(username);
+    auto it = chatRegister.chats.find(username);
+
+    if (it != chatRegister.chats.end()) {
+        chatRegister.chats.erase(it);
+        return true;  // La chat è stata rimossa con successo
+    } else {
+        return false;  // La chat con questo utente non esiste
+    }
 }
 
 void User::sendMessage(const User &addressee, const std::string &text) {
     auto &chat = getChat(addressee);
     chat.addMessage(Message(nick, addressee.nick, text));
-    
+
 }
 
 Chat &User::getChat(const User &otherUser) const {
