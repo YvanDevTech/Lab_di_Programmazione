92fb44ccd184b17def96f57b7c12c7d285191c57
YvanDevTech <yvantakoukam@gmail.com>
2024-01-17 12:24:56 +0100

    Aggiornamento dello unit testing e alcuni file per la compilazione del progetto

--- main.cpp
+++ main.cpp
@@ -1,32 +1,78 @@
 #include <iostream>
 #include "User.h"
+#include "Chat.h"
 
 int main() {
     User *yvan = new User("YvanTonga");
-    User *jordan = new User("JordanLoic");
     User *michelle = new User("MichelleStella");
-    User *ingrid = new User("IngridJulienne");
+    User *jordan = new User("JordanLoic");
+    User *ingrid = new User("IngridJulie");
+
+
+
+    jordan->startNewChat(*ingrid);
+    std::cout << "Chat started between " << jordan->getNick() << " and " << ingrid->getNick() << std::endl;
+
+    std::cout << jordan->getNick() << " sent a message to " << ingrid->getNick() << std::endl;
+    jordan->sendMessage(*ingrid, " Che ne pensi di andare in vacanza a Londra ?");
+
+    const std::shared_ptr<Chat> chatfromJordanIngrid = jordan->getChat(*ingrid);
+    if (chatfromJordanIngrid) {
+        int messageCount = chatfromJordanIngrid->getMessageCount();
+        if (messageCount > 0) {
+            // Esempio di lettura di un messaggio
+            chatfromJordanIngrid->readMessage(0, std::cout);
+        } else {
+            std::cout << "La chat è vuota. Nessun messaggio da leggere." << std::endl;
+        }
+    } else {
+        std::cout << "La chat non esiste." << std::endl;
+    }
+
+    // Ingrid risponde a Jordan
+    ingrid->sendMessage(*jordan, "Si, sarebbe fantastico! Londra e una citta meravigliosa.");
+
+// Leggi il messaggio di Ingrid
+    if (chatfromJordanIngrid) {
+        int messageCount = chatfromJordanIngrid->getMessageCount();
+        if (messageCount > 0) {
+            // Esempio di lettura di un messaggio
+            chatfromJordanIngrid->readMessage(1, std::cout);
+
+        } else {
+            std::cout << "La chat è vuota. Nessun messaggio da leggere." << std::endl;
+        }
+    } else {
+        std::cout << "La chat non esiste." << std::endl;
+    }
 
 
     yvan->startNewChat(*michelle);
+    std::cout << "Chat started between " << yvan->getNick() << " and " << michelle->getNick() << std::endl;
+
     yvan->sendMessage(*michelle, " Sto pensando di visitare Firenze. Non ci sono mai stato e voglio vedere i suoi famosi monumenti.");
-    michelle->sendMessage(*yvan, " Sarà una bellissima esperienza! Assicurati di visitare il Duomo e il Ponte Vecchio.");
-    yvan->startNewChat(*jordan);
-    yvan->sendMessage(*jordan," Sai, ho appena finito di leggere un libro davvero interessante. Ne hai letto qualcosa di recente?");
-    jordan->sendMessage(*yvan, "Oh, che bello! Mi piace leggere, ma negli ultimi tempi non ho avuto molto tempo. Qual era il libro?");
+    std::cout << yvan->getNick() << " sent a message to " << michelle->getNick() << std::endl;
 
-    jordan->startNewChat(*michelle);
-    jordan->sendMessage(*michelle," Ho avuto una giornata impegnativa al lavoro, ma ora sono a casa e sto rilassandomi un po'. Hai piani per il weekend?");
-    michelle->sendMessage(*jordan, "Nulla di speciale, probabilmente passero del tempo con la mia famiglia. E tu?");
+    // Verifica se ci sono messaggi nella chat prima di leggere
+    const std::shared_ptr<Chat> chatfromYvan = yvan->getChat(*michelle);
 
-    michelle->startNewChat(*ingrid);
-    michelle->sendMessage(*ingrid, "Anche io sto bene, grazie! Hai fatto qualcosa di interessante oggi?");
-    ingrid->sendMessage(*michelle, "Si, sono andato al parco con il mio cane. È stato divertente!");
-    michelle->removeChat("YvanTonga");
-    yvan->removeChat("JordanLoic");
+    if (chatfromYvan) {
+        int messageCount = chatfromYvan->getMessageCount();
+        if (messageCount > 0) {
+            // Esempio di lettura di un messaggio
+            chatfromYvan->readMessage(0, std::cout);
+        } else {
+            std::cout << "La chat è vuota. Nessun messaggio da leggere." << std::endl;
+        }
+    } else {
+        std::cout << "La chat non esiste." << std::endl;
+    }
+
 
     delete michelle;
-    delete jordan;
     delete yvan;
+    delete jordan;
     delete ingrid;
+
+    return 0;
 }
